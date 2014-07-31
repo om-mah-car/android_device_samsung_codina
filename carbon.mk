38a88bff0cbffa28555ce1246e31847f84d70fe3
Adam Swietek <judas77@gazeta.pl>
2014-07-31 23:36:01 +0200

    carbon initial - fix conflicts

--- carbon.mk
+++ carbon.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GT-I8160
@@ -8,14 +8,11 @@ PRODUCT_RELEASE_NAME := GT-I8160
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/codina/full_codina.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_codina
+PRODUCT_NAME := carbon_codina
 PRODUCT_DEVICE := codina
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I8160
